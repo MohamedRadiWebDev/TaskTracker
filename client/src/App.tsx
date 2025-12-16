4a771f0a13e839b6a1451b51735babf52757f789
MohamedRadiWebDev <141498530+MohamedRadiWebDev@users.noreply.github.com>
2025-12-16 03:38:38 +0200

    Add receipt distribution page with drag-fill support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,16 @@ import { Toaster } from "./components/ui/toaster";
 import { TooltipProvider } from "./components/ui/tooltip";
 import MissionManagement from "./pages/mission-management";
 import PeriodReport from "./pages/period-report";
+import TaskSearch from "./pages/task-search";
+import ReceiptDistributionPage from "./pages/receipt-distribution";
 import NotFound from "./pages/not-found";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={MissionManagement} />
+      <Route path="/search" component={TaskSearch} />
+      <Route path="/receipt-distribution" component={ReceiptDistributionPage} />
       <Route path="/period-report" component={PeriodReport} />
       <Route component={NotFound} />
     </Switch>
